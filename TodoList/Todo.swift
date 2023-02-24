cff3ec07d87a66b2a5d2053bcb5e5cadaf20f9c8
Samar Salem <2x.samar@gmail.com>
2023-02-24 10:17:14 +0400

    Add Todo Struct

---
+++ TodoList/Todo.swift
@@ -0,0 +1,14 @@
+//
+//  Todo.swift
+//  TodoList
+//
+//  Created by samar salem on 24/02/2023.
+//
+
+import Foundation
+
+//store information
+struct Todo{
+    var title : String
+    var isCompleted = false
+}
