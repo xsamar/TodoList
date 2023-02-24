2ea876e4e1bdc95dba455977ab1c143f0beaa439
Samar Salem <2x.samar@gmail.com>
2023-02-24 10:13:09 +0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by samar salem on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
