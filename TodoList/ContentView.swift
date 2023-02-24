44f10e63f187f0e8aec9b20c6332501513cf2cb6
Samar Salem <2x.samar@gmail.com>
2023-02-24 10:32:16 +0400

    Added Navigation Stack

--- TodoList/ContentView.swift
+++ TodoList/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var todos = [Todo(title: "Buy some groceries"),
-                 Todo(title: "Oick up sister from school"),
+                 Todo(title: "Pick up sister from school"),
                  Todo(title: "Prepare for class", isCompleted: true)]
     
     var body: some View {
-        List(todos) { todo in
-            Text(todo.title)
+        NavigationStack{
+            List(todos) { todo in
+                Text(todo.title)
+            }
+            .navigationTitle("Todos")
         }
+        
     }
 }
 
